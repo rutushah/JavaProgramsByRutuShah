46dac040caa545e50d051fa45f700d74706cf0ee
rutushah <36071899+rutushah@users.noreply.github.com>
2021-03-21 13:57:27 +0530

    second commit

--- src/javaFundamentals/IfExample.java
+++ src/javaFundamentals/IfExample.java
@@ -17,7 +17,22 @@ public class IfExample {
 //            System.out.println("Not a leap year");
 //        }
 
-        int grade = 75;
+        int score = 75;
+        char grade;
+
+        if (score >= 90){
+            grade = 'A';
+        }else if (score >= 80){
+            grade = 'B';
+        }else  if (score >= 70){
+            grade = 'c';
+        }else if (score >= 60){
+            grade = 'D';
+        }else {
+            grade = 'F';
+        }
+
+        System.out.println("Grade is : " + grade);
 
     }
 }
